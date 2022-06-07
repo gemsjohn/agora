8b6a42d1aff4336370e00f34953fd0afeeae379c
JRM <johnrhmartin@gmail.com>
2022-06-06 22:07:29 -0400

    built in manual media queries to handle different size screens specifically targeting the carousel

--- client/src/components/ViewableListing.js
+++ client/src/components/ViewableListing.js
@@ -23,6 +23,25 @@ let imageArray2 = [];
 function ViewableListing() {
     // const [isShownLogin, setIsShownLogin] = useState(false);
     // const [isShownSignup, setIsShownSignup] = useState(false);
+    let denominator;
+    if (window.innerWidth <= 480) {
+        denominator = 5
+    } else if (window.innerWidth >= 481 && window.innerWidth <= 768) {
+        denominator = 7
+    } else if (window.innerWidth >= 769 && window.innerWidth <= 1024) {
+        denominator = 15
+    } else if (window.innerWidth >= 1025 && window.innerWidth <= 1200) {
+        denominator = 22
+    }else if (window.innerWidth >= 1201) {
+        denominator = 35
+    }
+    function getBaseLog(x, y) {
+        return Math.log(y) / Math.log(x);
+    }
+    const state = {
+        
+        width: (window.innerWidth / denominator)
+    };
     
     const media = localStorage.getItem('listingMedia');
     const title = localStorage.getItem('listingTitle');
@@ -34,17 +53,19 @@ function ViewableListing() {
     imageArray1 = JSON.parse(media);
 
     function ImageDisplay() {
-        var settings = {
+        
+        const settings = {
             dots: true,
             infinite: true,
             speed: 500,
             slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToScroll: 1,
+            
         };
         for (let i = 0; i < imageArray1.length; i++) {
             imageArray2[i] =
                 <div>
-                    <img src={imageArray1[i]} />
+                    <img src={imageArray1[i]} style={{ height: 'auto', width: state.width + "vw"}}/>
                 </div>
         }
 
@@ -54,12 +75,12 @@ function ViewableListing() {
             </Slider>
         )
     }
-    console.log(ImageDisplay())
+    console.log(window.innerWidth)
     
     return (
         <div className="App">
             <div className="App-header" style={{ justifyContent: 'start' }}>
-            <div className="container" style={{ marginTop: '4vh'}}>
+            <div className="container" style={{ marginTop: '4vh', display: 'true', width: state.width + "vw"}}>
                 <ImageDisplay />
             </div>
             <Table striped bordered hover variant="dark" style={{ margin: '4vh 0 10vh 0' }}>
